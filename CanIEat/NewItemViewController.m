996ad7ba0b8f3f74d151598ea53ede0856f25654
jlnbuiles <jlnbuiles@gmail.com>
2011-08-02 22:14:00 -0400

    yessss

--- CanIEat/NewItemViewController.m
+++ CanIEat/NewItemViewController.m
@@ -1,5 +1,5 @@
 #import "NewItemViewController.h"
-// if this is working then this text should show up
+// if this is working then this text should show upppppppp
 @interface NewItemViewController()
 //- (void)setALocale;
 @end
